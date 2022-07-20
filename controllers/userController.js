fecaab3153e81914171b08d0e45f978285482a02
shingchan95 <chanhei888@gmail.com>
2022-07-20 22:28:39 +0100

    add friends route in userRoute

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,12 +9,7 @@ module.exports = {
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .select('-__v')
-      .populate({
-        path: 'thoughts'
-      })
-      .populate({
-        path: 'friend'
-      })
+      // populate('thoughts') populate('friend')
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,9 +9,11 @@ const {
   
 } = require('../../controllers/userController');
 
+const {addfriends, deletefriends} = require('../../controllers/friendsController');
 
 router.route('/').get(getallUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
+router.route('/addfriend/:userID').post(addfriends).delete(deletefriends)
 
 
 
