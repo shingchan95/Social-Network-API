23040a9b23409ade0ab21f4e52fd07ef23c6f7ea
shingchan95 <chanhei888@gmail.com>
2022-07-23 20:44:05 +0100

    update on populate

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,9 +8,14 @@ module.exports = {
   },
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-      .select('-__v')
-      .populate('thoughts') 
-      .populate('friends')
+    .populate({
+      path: 'thoughts',
+      select: '-__v'
+    })
+    .populate({
+      path: 'friends',
+      select: '-__v'
+    })
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
